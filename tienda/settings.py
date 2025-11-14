87a63efcd3d33f17d08861d8ca793e2927afa7e6
AXEL CASTILLO <acastilloy2@miumg.edu.gt>
2025-11-13 21:13:03 -0600

    carga de archivos

--- tienda/settings.py
+++ tienda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t*6kpybgveof-+43a5vn7=m9on@)ws=&+qqi$c32sl5&n!(#&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
